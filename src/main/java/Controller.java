6901918ba2b9e55926241008c0225a3bfed181d1
AfonsoCostaVale <afonsocostavale@protonmail.com>
2021-03-17 16:13:06 +0000

    Added javaFx bare-bones

---
+++ src/main/java/Controller.java
@@ -0,0 +1,8 @@
+import javafx.fxml.FXML;
+
+public class Controller {
+    @FXML private void teste(){
+        System.out.println("teste");
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,27 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import readers.ExelReader;
 import readers.JavaReader;
 
-public class Main {
+public class Main extends Application {
+
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("Painel de utilização");
+        Scene scene =new Scene(root, 700, 800);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
         new JavaReader("/home/main/IdeaProjects/ES-2Sem-2021-Grupo24/src/main/java/Main.java").read();
         new ExelReader("/home/main/Downloads/testeES.xlsx").read();
+
+        launch(args);
     }
 
 }
