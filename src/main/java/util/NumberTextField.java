a1fbdc7ab8fd6ba7fd66f110931a7f7c011014cf
rscro-iscte <rscro@iscte-iul.pt>
2021-05-05 15:09:29 +0100

    Added JavaDoc and cleaned up NumberTextField.

--- src/main/java/util/NumberTextField.java
+++ src/main/java/util/NumberTextField.java
@@ -3,35 +3,55 @@ package util;
 import javafx.scene.control.TextField;
 
 /**
- *
+ * Numeric textfield for JavaFX
  */
 public class NumberTextField extends TextField {
 
     /**
-     *
+     * Warning to only type in numeric values
      */
     public NumberTextField() {
         this.setPromptText("Enter only numbers");
     }
 
     /**
-     * @param i
-     * @param i1
-     * @param string
+     * @param start start of selected text
+     * @param end end of selected text
+     * @param text numeric text to replace text
      */
     @Override
-    public void replaceText (int i, int i1, String string) {
-        if(string.matches("[0-9]") || string.isEmpty()) {
-            super.replaceText(i,i1,string);
+    public void replaceText(int start, int end, String text)
+    {
+        if (validate(text))
+        {
+            super.replaceText(start, end, text);
         }
     }
 
     /**
-     * @param string
+     * @param text numeric text to replace selection
      */
     @Override
-    public void replaceSelection(String string) {
-        super.replaceSelection(string);
+    public void replaceSelection(String text)
+    {
+        if (validate(text))
+        {
+            super.replaceSelection(text);
+        }
+    }
+
+    /**
+     *
+     * @param text text to check
+     * @return true if text is numeric
+     */
+    private boolean validate(String text)
+    {
+        if (text.matches("[0-9]") || text == "")
+        {
+            return true;
+        }
+        return false;
     }
 
 }
