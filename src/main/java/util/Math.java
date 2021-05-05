ba0ab6d41686c1bb178374ba27f5dea8a3628c1d
hfcbf <hfcbf@iscte-iul.pt>
2021-05-05 11:57:34 +0100

    MathTest class created All tests for the class Math created in the new class

--- src/main/java/util/Math.java
+++ src/main/java/util/Math.java
@@ -2,36 +2,36 @@ package util;
 
 public class Math {
 
-    public static int calculateTotal(int trueP, int trueN, int falseP, int falseN){
-        int total = trueP + trueN + falseN + falseP;
+    public static double calculateTotal(double trueP, double trueN, double falseP, double falseN){
+        double total = trueP + trueN + falseN + falseP;
         return total;
     }
 
-    public static Double calculatePrecision(int trueP, int falseP){
+    public static Double calculatePrecision(double trueP, double falseP){
         double precision = trueP / (trueP + falseP);
         return precision;
     }
 
-    public static Double calculateRecall(int trueP, int falseN){
+    public static Double calculateRecall(double trueP, double falseN){
         double recall = trueP / (trueP + falseN);
         return recall;
     }
 
-    public static Double calculateError(int trueP, int trueN, int falseP, int falseN){
-        int total = calculateTotal(trueP, trueN, falseP, falseN);
+    public static Double calculateError(double trueP, double trueN, double falseP, double falseN){
+        double total = calculateTotal(trueP, trueN, falseP, falseN);
         double error = (falseN + falseP) / total;
 
         return error;
     }
 
-    public static Double calculateAccuracy(int trueP, int trueN, int falseP, int falseN){
-        int total = calculateTotal(trueP, trueN, falseP, falseN);
+    public static Double calculateAccuracy(double trueP, double trueN, double falseP, double falseN){
+        double total = calculateTotal(trueP, trueN, falseP, falseN);
         double accuracy = (trueN + trueP) / total;
 
         return accuracy;
     }
 
-    public String evaluateMeasure(Double measure) {
+    public static String evaluateMeasure(Double measure) {
         if (measure == 0) {
             return "Worst";
         } else if (measure > 0 && measure < 0.5) {
